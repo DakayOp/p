07e115732b3b17b6fa4247a7e5516ebce6540cbe
DakayOp <162329003+DakayOp@users.noreply.github.com>
2024-05-20 16:45:26 +0700

    Update cron.py

--- cron.py
+++ cron.py
@@ -4,7 +4,7 @@ from crontab import CronTab
 cron = CronTab(user='root')  # Ganti 'username' dengan nama pengguna Anda
 
 # Buat objek cron job baru
-job = cron.new(command='python /workspace/p/scrape.py')  # Ganti '/path/to/anjai.py' dengan path yang sesuai
+job = cron.new(command='python3 /workspace/p/scrape.py')  # Ganti '/path/to/anjai.py' dengan path yang sesuai
 
 # Atur jadwal cron job untuk menjalankan setiap 1 menit
 job.minute.every(1)
